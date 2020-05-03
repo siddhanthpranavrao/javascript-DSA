e55c1b847c0b7ffd6dc9cbf411fbcaa6a39ac703
Siddhanth Pranav Rao <63382849+siddhanthpranavrao@users.noreply.github.com>
2020-05-03 12:20:59 +0530

    Add files via upload

---
+++ isSubsequence_v2.js
@@ -0,0 +1,12 @@
+function isSubsequence(str1, str2) {
+  if(str1.length === 0) return true
+  if(str2.length === 0) return false
+  if(str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1))  
+  return isSubsequence(str1, str2.slice(1))
+}
+
+console.log(isSubsequence('hello', 'hello world'));
+console.log(isSubsequence('sing', 'sting'));
+console.log(isSubsequence('abc', 'abracadabra'));
+console.log(isSubsequence('abc', 'acb'));
+console.log(isSubsequence('', 'acb'));
