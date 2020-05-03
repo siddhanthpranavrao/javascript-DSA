4edbdcecea6fe0a5b034b531f0fa14379f7be7cd
Siddhanth Pranav Rao <63382849+siddhanthpranavrao@users.noreply.github.com>
2020-05-03 12:21:51 +0530

    Add files via upload

---
+++ maxSubArraySum_v2.js
@@ -0,0 +1,25 @@
+function maxSubArraySum(arr, num) {
+	let maxSum = 0;
+	let tempSum = 0;
+
+	if (arr.length < num) return null;
+
+	for (let i = 0; i < num; i++) {
+		maxSum += arr[i];
+	}
+
+	tempSum = maxSum;
+
+	for (let i = num; i < arr.length; i++) {
+		tempSum = tempSum - arr[i - num] + arr[i];
+		maxSum = Math.max(maxSum, tempSum);
+	}
+
+	return maxSum;
+}
+
+console.log(maxSubArraySum([ 1, 2, 5, 2, 8, 1, 5 ], 2));
+console.log(maxSubArraySum([ 1, 2, 5, 2, 8, 1, 5 ], 4));
+console.log(maxSubArraySum([ 4, 2, 1, 6 ], 1));
+console.log(maxSubArraySum([ 4, 2, 1, 6, 2 ], 4));
+console.log(maxSubArraySum([], 4));
